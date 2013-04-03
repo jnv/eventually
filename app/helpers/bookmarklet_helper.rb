019389ceb72ebb703013778ebef3ad0769a7413f
Jan Vlnas <git@jan.vlnas.cz>
2013-04-03 13:19:35 +0200

    Add invocation script From https://github.com/oliverfriedmann/rails-bookmarklet/

---
+++ app/helpers/bookmarklet_helper.rb
@@ -0,0 +1,20 @@
+module BookmarkletHelper
+  def compile_invocation_script(url, options = {})
+    default_error_message = "Something went wrong with the bookmarklet."
+    error_message = options.has_key?(:error_message) ? options[:error_message] : default_error_message
+
+    full_url = url
+    if options.has_key?(:params)
+      options[:params].each do |key,value|
+        if !((full_url.ends_with? "?") || (full_url.ends_with? "&"))
+          full_url += (full_url.include? "?") ? "&" : "?"
+        end
+        full_url += key + "=" + value
+      end
+    end
+
+    return "javascript:(function(){var d=document,z=d.createElement('scr'+'ipt'),b=d.body;try{" +
+      "if(!b)throw(0);z.setAttribute('src','" + full_url + "');b.appendChild(z);}" +
+      "catch(e){alert('" + error_message + "');}}).call(this);"
+  end
+end
